b4a27b95218faa38863d440de435adb09c2af2c2
Sebastian Morales <sebasandres.1547@gmail.com>
2026-03-19 16:43:47 -0500

    feat: implementar menu base en la capa view

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,54 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+
+    static void main(String[] args) {
+
+        Main obj = new Main();
+        obj.menu();
 
         }
+
+
+        public void menu(){
+
+            char OP='S';
+            int opcion=0;
+
+            Scanner Scanner = new Scanner(System.in);
+
+            while(OP=='S') {
+                System.out.print("""
+                    1. Registrar vehiculo
+                    2. Registrar conductor
+                    3. Registrar pasajero 
+                    4. Vender tickets
+                    5. Ver tickets
+                    6. Estadisticas
+                    
+                    0. Salir
+                    """);
+
+                System.out.print("Selecciona una opccion: ");
+                opcion = Scanner.nextInt();
+
+                switch (opcion){
+                    case 0:
+                        OP='N';
+                        System.out.println("Hemos terminado...");
+                        break;
+
+                    default:
+                        System.out.println("Opcion invalida");
+
+                }
+
+
+            }
+
+
     }
 }
