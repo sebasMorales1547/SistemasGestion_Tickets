61e9feaf31c3d00e40d6cc9747fd99670320fc04
Juan Sierra <jdiegosierra@unicesar.edu.co>
2026-03-19 19:49:21 -0500

    feat(model): se agrego la clase abstracta pasajera y la subclase pasajeroRegular

---
+++ src/main/java/sistemaGestion_Tickets/model/pasajeroRegular.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaGestion_Tickets.model;
+
+/**
+ *
+ * @author PC
+ */
+public class pasajeroRegular extends pasajero {
+
+    public pasajeroRegular(String cedula, String nombre) {
+        super(cedula, nombre);
+    }
+    
+    @Override
+    public double calcularDescuento(){
+        return 0.0;
+    }
+    
+    @Override
+    public String getTipo(){
+        return "Regular";
+    }
+    
+    @Override
+    public void imprimirDetalle(){
+        System.out.println("===Pasajero===");
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Cedula: " + cedula);
+        System.out.println("Tipo: Pasajero regular, no aplica descuento");
+    }
+    
+}
