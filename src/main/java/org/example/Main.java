8ed18438284b7d6ae4c21fb43b450f12124a371f
Sebastian Morales <sebasandres.1547@gmail.com>
2026-03-20 17:39:52 -0500

    fix: manejo de excepcion cuando el usuario ingresa letras en el menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,30 @@
 package org.example;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static void main(String[] args) {
+    static void main(String[] args) throws InterruptedException{
 
         Main obj = new Main();
         obj.menu();
 
-        }
+    }
 
+    public void menu() throws InterruptedException {
 
-        public void menu(){
+        char OP = 'S';
+        int opcion = 0;
 
-            char OP='S';
-            int opcion=0;
+        Scanner Scanner = new Scanner(System.in);
 
-            Scanner Scanner = new Scanner(System.in);
+        while (OP == 'S') {
 
-            while(OP=='S') {
-
-                System.out.print("""
+            System.out.println("""
+                    
+                    --------- MENU ---------
                     1. Registrar vehiculo
                     2. Registrar conductor
                     3. Registrar pasajero 
@@ -33,28 +35,30 @@ public class Main {
                     0. Salir
                     """);
 
-                System.out.print("Selecciona una opccion: ");
+            System.out.print("Selecciona una opccion: ");
+            try {
                 opcion = Scanner.nextInt();
 
-                switch (opcion){
+                switch (opcion) {
                     case 0:
-                        OP='N';
+                        OP = 'N';
                         System.out.println("Hemos terminado...");
                         break;
 
                     default:
                         System.err.println("Opcion invalida");
-                        try {
-                            Thread.sleep(2000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println("volviendo al menu...");
+                        Thread.sleep(2000);
+                        System.out.println("Regresando al menu...");
+
                 }
 
-
+            } catch (InputMismatchException e) {
+                System.err.println("Debe ingresar letras no numeros");
+                Scanner.nextLine();
+                Thread.sleep(2000);
+                System.out.println("Regresando al menu...");
             }
 
-
+        }
     }
 }
