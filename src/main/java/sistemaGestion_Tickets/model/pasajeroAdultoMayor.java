1dc88d8b36a7cfc5673ddbf277f85a83c8742089
Juan Sierra <jdiegosierra@unicesar.edu.co>
2026-03-19 20:05:21 -0500

    feat(model): se agregaron las subclases pasajeroEstudiante y pasajeroAdultoMayor y los metodos  sobre-escritos.

---
+++ src/main/java/sistemaGestion_Tickets/model/pasajeroAdultoMayor.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaGestion_Tickets.model;
+
+/**
+ *
+ * @author PC
+ */
+public class pasajeroAdultoMayor extends pasajero {
+
+    public pasajeroAdultoMayor(String cedula, String nombre) {
+        super(cedula, nombre);
+    }
+    
+    
+    @Override 
+    public double calcularDescuento(){
+        return 0.30;
+    }
+    
+    @Override 
+    public String getTipo(){
+        return "Adulto Mayor";
+    }
+    
+    @Override 
+    public void imprimirDetalle(){
+        System.out.println("===Pasajero===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cedula " + cedula);
+        System.out.println("Tipo: Adulto mayor, se le aplica un descuento del 30%");
+    }
+    
+    
+}
