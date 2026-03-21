07c7c9384c04b21f0d71c9f15db470db8d744798
Sebastian Morales <sebasandres.1547@gmail.com>
2026-03-21 11:02:13 -0500

    refactor: agregar registro de bus, buseta y microbus con persistencia en archivo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,10 @@
 package org.example;
+import model.Bus;
+import dao.VehiculoDao;
+import model.Buseta;
+import model.MicroBus;
+
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -6,7 +12,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+     static void main(String[] args) throws InterruptedException {
 
         Main obj = new Main();
         obj.menu();
@@ -44,6 +50,125 @@ public class Main {
                         OP = 'N';
                         System.out.println("Hemos terminado...");
                         break;
+                    case 1:
+                        menuVehiculos();
+                    default:
+                        System.err.println("Opcion invalida");
+                        Thread.sleep(2000);
+                        System.out.println("Regresando al menu...");
+
+                }
+
+            } catch (InputMismatchException e) {
+                System.err.println("Debe ingresar letras no numeros");
+                Scanner.nextLine();
+                Thread.sleep(2000);
+                System.out.println("Regresando al menu...");
+            }
+
+        }
+    }
+
+
+
+    public void menuVehiculos() throws InterruptedException{
+
+        char OP = 'S';
+        int opcion = 0;
+
+        Scanner Scanner = new Scanner(System.in);
+        VehiculoDao VehiculoDao = new VehiculoDao();
+
+        while (OP == 'S') {
+
+            System.out.println("""
+                    
+                    --------- MENU ---------
+                    1. Registrar bus
+                    2. Registrar buseta
+                    3. Registrar microbus
+                    
+                    0. Salir
+                    """);
+
+            System.out.print("Selecciona una opccion: ");
+            try {
+
+                opcion = Scanner.nextInt();
+
+                switch (opcion) {
+                    case 0:
+                        OP = 'N';
+                        System.out.println("Hemos terminado...");
+                        break;
+                    case 1:
+
+                        char continuar='S';
+                        do {
+
+                            String placa, ruta;
+                            System.out.println("*******REGISTRO DE BUS*******");
+                            Scanner.nextLine();
+
+                            System.out.print("Ingrese la placa del vehiculo: ");
+                            placa = Scanner.nextLine();
+
+                            System.out.print("Ingrese la ruta del vehiculo: ");
+                            ruta = Scanner.nextLine();
+
+                            Bus Bus = new Bus(placa, ruta);
+                            VehiculoDao.guardarVehiculo(Bus);
+
+                            System.out.println("¿Ingresar otro bus? S/N");
+                            continuar = Scanner.next().charAt(0);
+                        }while(continuar == 'S');
+                        break;
+
+                    case 2:
+
+
+                        do {
+
+                            String placa, ruta;
+                            System.out.println("*******REGISTRO DE BUSETA*******");
+                            Scanner.nextLine();
+
+                            System.out.print("Ingrese la placa del vehiculo: ");
+                            placa = Scanner.nextLine();
+
+                            System.out.print("Ingrese la ruta del vehiculo: ");
+                            ruta = Scanner.nextLine();
+
+                            Buseta Buseta = new Buseta(placa, ruta);
+                            VehiculoDao.guardarVehiculo(Buseta);
+
+                            System.out.println("¿Ingresar otro bus? S/N");
+                            continuar = Scanner.next().charAt(0);
+                        }while(continuar == 'S');
+                        break;
+
+                    case 3:
+
+                        do {
+
+                            String placa, ruta;
+                            System.out.println("*******REGISTRO DE BUSETA*******");
+                            Scanner.nextLine();
+
+                            System.out.print("Ingrese la placa del vehiculo: ");
+                            placa = Scanner.nextLine();
+
+                            System.out.print("Ingrese la ruta del vehiculo: ");
+                            ruta = Scanner.nextLine();
+
+                            MicroBus MicroBus = new MicroBus(placa,ruta);
+                            VehiculoDao.guardarVehiculo(MicroBus);
+
+                            System.out.println("¿Ingresar otro bus? S/N");
+                            continuar = Scanner.next().charAt(0);
+                        }while(continuar == 'S');
+                        break;
+
 
                     default:
                         System.err.println("Opcion invalida");
@@ -61,5 +186,12 @@ public class Main {
 
         }
     }
+
+
+
+
+
+
+
 }
 
